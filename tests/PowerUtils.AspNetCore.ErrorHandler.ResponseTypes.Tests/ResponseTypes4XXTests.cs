90912920abedcfe3597e710eb184ea84de7a3328
Nelson Nobre <technobre@gmail.com>
2022-08-29 23:14:17 +0100

    test: Add support a tests para multi frameworks

--- tests/PowerUtils.AspNetCore.ErrorHandler.ResponseTypes.Tests/ResponseTypes4XXTests.cs
+++ tests/PowerUtils.AspNetCore.ErrorHandler.ResponseTypes.Tests/ResponseTypes4XXTests.cs
@@ -1,111 +1,114 @@
-﻿using PowerUtils.AspNetCore.Attributes;
+﻿using FluentAssertions;
+using PowerUtils.AspNetCore.Attributes;
+using Xunit;
 
-namespace PowerUtils.AspNetCore.ErrorHandler.ResponseTypes.Tests;
-
-public class ResponseTypes4XXTests
+namespace PowerUtils.AspNetCore.ErrorHandler.ResponseTypes.Tests
 {
-    [Fact]
-    public void Constructor_Response400ProblemDetailsAttribute()
+    public class ResponseTypes4XXTests
     {
-        // Arrange && Act
-        var act = new Response400ProblemDetailsAttribute();
+        [Fact]
+        public void StatusCode400_Constructor_Response400ProblemDetailsAttribute()
+        {
+            // Arrange && Act
+            var act = new Response400ProblemDetailsAttribute();
 
 
-        // Assert
-        act.StatusCode.Should()
-            .Be(400);
+            // Assert
+            act.StatusCode.Should()
+                .Be(400);
 
-        act.Type.Should()
-            .Be(typeof(ProblemDetailsResponse));
-    }
+            act.Type.Should()
+                .Be(typeof(ProblemDetailsResponse));
+        }
 
-    [Fact]
-    public void Constructor_Response401ProblemDetailsAttribute()
-    {
-        // Arrange && Act
-        var act = new Response401ProblemDetailsAttribute();
-
-
-        // Assert
-        act.StatusCode.Should()
-            .Be(401);
-
-        act.Type.Should()
-            .Be(typeof(ProblemDetailsResponse));
-    }
-
-    [Fact]
-    public void Constructor_Response403ProblemDetailsAttribute()
-    {
-        // Arrange && Act
-        var act = new Response403ProblemDetailsAttribute();
+        [Fact]
+        public void StatusCode401_Constructor_Response401ProblemDetailsAttribute()
+        {
+            // Arrange && Act
+            var act = new Response401ProblemDetailsAttribute();
 
 
-        // Assert
-        act.StatusCode.Should()
-            .Be(403);
+            // Assert
+            act.StatusCode.Should()
+                .Be(401);
 
-        act.Type.Should()
-            .Be(typeof(ProblemDetailsResponse));
-    }
+            act.Type.Should()
+                .Be(typeof(ProblemDetailsResponse));
+        }
 
-    [Fact]
-    public void Constructor_Response404ProblemDetailsAttribute()
-    {
-        // Arrange && Act
-        var act = new Response404ProblemDetailsAttribute();
-
-
-        // Assert
-        act.StatusCode.Should()
-            .Be(404);
-
-        act.Type.Should()
-            .Be(typeof(ProblemDetailsResponse));
-    }
-
-    [Fact]
-    public void Constructor_Response409ProblemDetailsAttribute()
-    {
-        // Arrange && Act
-        var act = new Response409ProblemDetailsAttribute();
+        [Fact]
+        public void StatusCode403_Constructor_Response403ProblemDetailsAttribute()
+        {
+            // Arrange && Act
+            var act = new Response403ProblemDetailsAttribute();
 
 
-        // Assert
-        act.StatusCode.Should()
-            .Be(409);
+            // Assert
+            act.StatusCode.Should()
+                .Be(403);
 
-        act.Type.Should()
-            .Be(typeof(ProblemDetailsResponse));
-    }
+            act.Type.Should()
+                .Be(typeof(ProblemDetailsResponse));
+        }
 
-    [Fact]
-    public void Constructor_Response413ProblemDetailsAttribute()
-    {
-        // Arrange && Act
-        var act = new Response413ProblemDetailsAttribute();
-
-
-        // Assert
-        act.StatusCode.Should()
-            .Be(413);
-
-        act.Type.Should()
-            .Be(typeof(ProblemDetailsResponse));
-    }
-
-    [Fact]
-    public void Constructor_Response415ProblemDetailsAttribute()
-    {
-        // Arrange && Act
-        var act = new Response415ProblemDetailsAttribute();
+        [Fact]
+        public void StatusCode404_Constructor_Response404ProblemDetailsAttribute()
+        {
+            // Arrange && Act
+            var act = new Response404ProblemDetailsAttribute();
 
 
-        // Assert
-        act.StatusCode.Should()
-            .Be(415);
+            // Assert
+            act.StatusCode.Should()
+                .Be(404);
 
-        act.Type.Should()
-            .Be(typeof(ProblemDetailsResponse));
+            act.Type.Should()
+                .Be(typeof(ProblemDetailsResponse));
+        }
+
+        [Fact]
+        public void StatusCode409_Constructor_Response409ProblemDetailsAttribute()
+        {
+            // Arrange && Act
+            var act = new Response409ProblemDetailsAttribute();
+
+
+            // Assert
+            act.StatusCode.Should()
+                .Be(409);
+
+            act.Type.Should()
+                .Be(typeof(ProblemDetailsResponse));
+        }
+
+        [Fact]
+        public void StatusCode413_Constructor_Response413ProblemDetailsAttribute()
+        {
+            // Arrange && Act
+            var act = new Response413ProblemDetailsAttribute();
+
+
+            // Assert
+            act.StatusCode.Should()
+                .Be(413);
+
+            act.Type.Should()
+                .Be(typeof(ProblemDetailsResponse));
+        }
+
+        [Fact]
+        public void StatusCode415_Constructor_Response415ProblemDetailsAttribute()
+        {
+            // Arrange && Act
+            var act = new Response415ProblemDetailsAttribute();
+
+
+            // Assert
+            act.StatusCode.Should()
+                .Be(415);
+
+            act.Type.Should()
+                .Be(typeof(ProblemDetailsResponse));
+        }
     }
 }
