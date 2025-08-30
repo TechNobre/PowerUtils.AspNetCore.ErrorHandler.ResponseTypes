70190cb6978fa97fff5ce9c90c064b850814524d
Nelson Nobre <technobre@gmail.com>
2025-08-30 13:03:56 +0100

    fix: Marked all implementations as `Obsolete`
    BREAKING CHANGE: Deprecated package

--- src/Response403ProblemDetailsAttribute.cs
+++ src/Response403ProblemDetailsAttribute.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PowerUtils.AspNetCore.ErrorHandler;
 
@@ -7,6 +8,7 @@ namespace PowerUtils.AspNetCore.Attributes
     /// <summary>
     /// Problem details response with status code 403 (Forbidden)
     /// </summary>
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public class Response403ProblemDetailsAttribute : ProducesResponseTypeAttribute
     {
         public Response403ProblemDetailsAttribute() : base(typeof(ErrorProblemDetails), StatusCodes.Status403Forbidden) { }
--- src/Response415ProblemDetailsAttribute.cs
+++ src/Response415ProblemDetailsAttribute.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PowerUtils.AspNetCore.ErrorHandler;
 
@@ -7,6 +8,7 @@ namespace PowerUtils.AspNetCore.Attributes
     /// <summary>
     /// Problem details response with status code 415 (UnsupportedMediaType)
     /// </summary>
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public class Response415ProblemDetailsAttribute : ProducesResponseTypeAttribute
     {
         public Response415ProblemDetailsAttribute() : base(typeof(ErrorProblemDetails), StatusCodes.Status415UnsupportedMediaType) { }
