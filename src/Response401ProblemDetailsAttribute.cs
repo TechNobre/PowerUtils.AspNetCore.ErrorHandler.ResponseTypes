32699b9c0aa03146ae152dc22ccc28c5151db921
Nelson Nobre <technobre@gmail.com>
2025-08-30 13:03:56 +0100

    fix: Marked all implementations as `Obsolete`
    BREAKING CHANGE: Deprecated package

--- src/Response401ProblemDetailsAttribute.cs
+++ src/Response401ProblemDetailsAttribute.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PowerUtils.AspNetCore.ErrorHandler;
 
@@ -7,6 +8,7 @@ namespace PowerUtils.AspNetCore.Attributes
     /// <summary>
     /// Problem details response with status code 401 (Unauthorized)
     /// </summary>
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public class Response401ProblemDetailsAttribute : ProducesResponseTypeAttribute
     {
         public Response401ProblemDetailsAttribute() : base(typeof(ErrorProblemDetails), StatusCodes.Status401Unauthorized) { }
--- src/Response404ProblemDetailsAttribute.cs
+++ src/Response404ProblemDetailsAttribute.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PowerUtils.AspNetCore.ErrorHandler;
 
@@ -7,6 +8,7 @@ namespace PowerUtils.AspNetCore.Attributes
     /// <summary>
     /// Problem details response with status code 404 (NotFound)
     /// </summary>
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public class Response404ProblemDetailsAttribute : ProducesResponseTypeAttribute
     {
         public Response404ProblemDetailsAttribute() : base(typeof(ErrorProblemDetails), StatusCodes.Status404NotFound) { }
